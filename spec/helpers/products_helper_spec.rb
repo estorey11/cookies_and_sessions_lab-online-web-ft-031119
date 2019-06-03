33c30a93cd926f2d630d9d4f5f107f97a62ca34e
Eric Storey <estorey@gmail.com>
2019-06-03 21:40:21 +0000

    Automatically backed up by Learn

--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -7,9 +7,4 @@ require 'rails_helper'
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe ProductsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
